b4661a0c066daf4a8ddecc24f1804a78871a621f
Fahima-anny <fahima.info03@gmail.com>
2024-03-14 12:41:14 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+document.getElementById("surahFatihaBtn").addEventListener("click",function(){
+    document.getElementById("suarhFatiha").style.display = "block"
+
+    document.getElementById("crossBtn").addEventListener("click",function(){
+        document.getElementById("suarhFatiha").style.display = "none"
+    })
+
+})
+document.getElementById("surahNaasBtn").addEventListener("click",function(){
+    document.getElementById("surahNaas").style.display = "block"
+
+    document.getElementById("crossBtn2").addEventListener("click",function(){
+        document.getElementById("surahNaas").style.display = "none"
+    })
+})
+
+document.getElementById("surahFalaqBtn").addEventListener("click",function(){
+    document.getElementById("surahFalaq").style.display = "block"
+
+    document.getElementById("crossBtn3").addEventListener("click",function(){
+        document.getElementById("surahFalaq").style.display = "none"
+    })
+})
+document.getElementById("surahIkhlasBtn").addEventListener("click",function(){
+    document.getElementById("surahIkhlas").style.display = "block"
+
+    document.getElementById("crossBtn4").addEventListener("click",function(){
+        document.getElementById("surahIkhlas").style.display = "none"
+    })
+})
+document.getElementById("surahLahabBtn").addEventListener("click",function(){
+    document.getElementById("surahLahab").style.display = "block"
+
+    document.getElementById("crossBtn5").addEventListener("click",function(){
+        document.getElementById("surahLahab").style.display = "none"
+    })
+})
+
+document.getElementById("surahNasrBtn").addEventListener("click",function(){
+    document.getElementById("surahNasr").style.display = "block"
+
+    document.getElementById("crossBtn6").addEventListener("click",function(){
+        document.getElementById("surahNasr").style.display = "none"
+    })
+})
+
+document.getElementById("surahKafirunBtn").addEventListener("click",function(){
+    document.getElementById("surahKafirun").style.display = "block"
+
+    document.getElementById("crossBtn7").addEventListener("click",function(){
+        document.getElementById("surahKafirun").style.display = "none"
+    })
+})
+
+document.getElementById("surahKawsarBtn").addEventListener("click",function(){
+    document.getElementById("surahKawsar").style.display = "block"
+
+    document.getElementById("crossBtn8").addEventListener("click",function(){
+        document.getElementById("surahKawsar").style.display = "none"
+    })
+})
+
+document.getElementById("surahMaunBtn").addEventListener("click",function(){
+    document.getElementById("surahMaun").style.display = "block"
+
+    document.getElementById("crossBtn9").addEventListener("click",function(){
+        document.getElementById("surahMaun").style.display = "none"
+    })
+})
+
+document.getElementById("surahKuraishBtn").addEventListener("click",function(){
+    document.getElementById("surahKuraish").style.display = "block"
+
+    document.getElementById("crossBtn10").addEventListener("click",function(){
+        document.getElementById("surahKuraish").style.display = "none"
+    })
+})
+
+
+
+
+
+
+
+
+
+
